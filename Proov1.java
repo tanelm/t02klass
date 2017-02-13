a504845a13b4ae7b0e91fe2c69db09fc081f6a24
ukupode <ukupode@greeny.cs.tlu.ee>
2017-02-13 12:48:24 +0200

    kommentaar juurde mis teeb

--- Proov1.java
+++ Proov1.java
@@ -8,3 +8,12 @@ public class Proov1{
 		System.out.println(pitsaloik3.toString());
 	}
 }
+/*
+
+[ukupode@greeny t02klass]$ java Proov1
+Kolmnurk alusega:5.0m ja kõrgusega:7.0m, ymbermoot 19.866068747318508m, pindala 17.5 ruutmeetrit.
+Kolmnurk alusega:4.0m ja kõrgusega:3.0m, ymbermoot 11.21110255092798m, pindala 6.0 ruutmeetrit.
+Kolmnurk alusega:11.0m ja kõrgusega:0.5m, ymbermoot 22.04536101718726m, pindala 2.75 ruutmeetrit.
+
+
+*/
